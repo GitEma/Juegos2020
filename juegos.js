a2b4981698e870472b558bf2b458dc64b6fca3a1
GitEma <68866131+GitEma@users.noreply.github.com>
2020-08-05 19:10:34 -0300

    Update juegos.js
    Se corrigieron errores varios.

--- juegos.js
+++ juegos.js
@@ -2,24 +2,24 @@ var juegosArray = [];
 
 function showJuegos(array) {
 
-    let contenido = "";
+    let htmlContentToAppend = "";
     for (let i = 0; i < array.length; i++) {
         let juegos = array[i];
 
-        contenido += `
+        htmlContentToAppend += `
          <p>Título: </p>` + juegos.titulo + `<br>
         <p>Plataforma(s): </p>` + juegos.plataforma + `<br>
         <p>Fecha de lanzamiento: </p>` + juegos.fecha + `<br>
         <p>Género: </p>` + juegos.genero + `<br>
         <br><hr><br>`
-        
-        document.getElementById('catalago').innerHTML = contenido;
-    }
-};
 
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONDATA(JUEGOS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
+        document.getElementById("informacion").innerHTML = htmlContentToAppend;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONDATA(JUEGOS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") 
         {
             juegosArray = resultObj.data;
 

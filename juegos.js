c7d62b72e43d26ccc3423cc38e949d91523c892f
GitEma <68866131+GitEma@users.noreply.github.com>
2020-08-05 17:41:21 -0300

    Commit inicial

---
+++ juegos.js
@@ -0,0 +1,28 @@
+var juegosArray = [];
+
+function showJuegos(array) {
+
+    let contenido = "";
+    for (let i = 0; i < array.length; i++) {
+        let juegos = array[i];
+
+        contenido += 'Título: ' + juegos.nombre + '<br>';
+        contenido += 'Plataforma(s): ' + juegos.plataforma + '<br>';
+        contenido += 'Fecha de lanzamiento: ' + juegos.fecha + '<br>';
+        contenido += 'Género: ' + juegos.genero + '<br>';
+        contenido += '<br><hr><br>'
+
+        document.getElementById('catalago').innerHTML = contenido ;
+    }
+};
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONDATA(JUEGOS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            juegosArray = resultObj.data;
+
+            showJuegos(juegosArray);
+        }
+    });
+});

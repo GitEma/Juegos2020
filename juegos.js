52dfd28d2c4e116bbaa5400caa6747b3487c7d36
GitEma <68866131+GitEma@users.noreply.github.com>
2020-08-07 23:40:26 -0300

    Update juegos.js
    acomodando el htmlContentToApend

--- juegos.js
+++ juegos.js
@@ -15,9 +15,9 @@ function showJuegos(array) {
                         <div class="d-flex w-100 justify-content-between">
                             <h4 class="mb-1">`+ juegos.titulo +`</h4>
                         </div>
-                        <p>Plataforma(s): ` + juegos.plataforma + `</p><br>
-                        <p>Fecha de lanzamiento: ` + juegos.fecha + `</p><br>
-                        <p>Género: ` + juegos.genero + `</p><br></div><br><hr>
+                        <p>Plataforma(s):<br> ` + juegos.plataforma + `</p><br>
+                        <p>Fecha de lanzamiento:<br> ` + juegos.fecha + `</p><br>
+                        <p>Género:<br> ` + juegos.genero + `</p><br></div><br><hr>
                     </div>
                 </div>`
 
